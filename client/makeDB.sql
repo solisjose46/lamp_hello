27668960327830d4f7478d8310dfdd16005cd797
Jose Salazar <solisjose466@gmail.com>
2021-07-21 21:20:30 -0700

    including php files

---
+++ client/makeDB.sql
@@ -0,0 +1,25 @@
+create table users(
+        userID int not null auto_increment,
+        username varchar(50) not null,
+        password varchar(255) not null,
+        createdAT datetime default current_timestamp,
+        primary key (userID)
+);
+
+create table following(
+        followID int not null auto_increment,
+	user1_ID int not null,
+	user2_ID int not null,
+	primary key (followID),
+	foreign key (user1_ID) references users (userID) on delete cascade on update cascade,
+	foreign key (user2_ID) references users (userID) on delete cascade on update cascade
+);
+
+create table borks(
+        borkID int not null auto_increment,
+        user_ID int not null,
+        bork varchar(140) not null,
+        createdAT datetime default current_timestamp,
+        primary key (borkID),
+	foreign key (user_ID) references users (userID) on delete cascade on update cascade
+);
